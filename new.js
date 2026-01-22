9e95473b48b8656b48550f5039224ffc2896f3c3
chandtushar984-boop <chandtushar984@gmail.com>
2026-01-22 11:21:05 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,44 @@
+// // const para1=document.getElementById("para")
+// // para1.innerText="this is a new paragraph"
+// // para1.style.color="green"
+// // console.log(para1)
+
+// // const para2=document.getElementsByClassName('para')
+// // para2[0].innerText="this is a new paragraph
+// // para2[1].style.color='blue'
+// // console.log(para2)
+//  const para2=document.getElementsByClassName("para")
+//  para2[0].innerText="This is a new program"
+//  para2[1].style.color='green'
+//  console.log(para2)
+
+//  const para=document.querySelectorAll(".para")
+//  para[0].innerText="this is a new paragraph"
+//  para[1].style.color="green"
+//  console.log(para )
+// const containe= document.querySelector( ".conrainer")
+// container.innerHTML="<h1>This is a Heading</h1>"
+// console.log(container)
+// const btn=document.querySelector("button")
+// btn.classList.add("btn")
+// btn.addEventListener('click',function(){alert("You have clisked the button")})
+// btn.removeEventListener('click',message)
+
+// function message(event)
+// {
+//     console.log(event.key)
+
+// }
+// const btn=document.querySelector("button")
+// btn.classList.add("btn")
+// btn.addEventListener('keyup',message)
+
+// const form=document.querySelector("form")
+// form.addEventListener('submit',function(event){
+//     event.preventDefault()
+//     console.log("Form Submitted")})
+
+
+const container=document.querySelector('.container')
+const outer=document.querySelector('.outer')
+const button=document.querySelector('button')
